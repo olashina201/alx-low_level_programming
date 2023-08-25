760dd92fd02b410716147aee17b022cb119dca83
olashina201 <olashina201@gmail.com>
2023-08-25 16:05:57 +0100

    update

--- 0x18-dynamic_libraries/3-strspn.c
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -15,5 +15,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	return (0);
+	unsigned int bytes = 0;
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
+	}
+
+	return (bytes);
 }
